521fa103c26bfa539854edea45aa47879709b213
Georgi957 <gesh007@abv.bg>
2024-02-02 10:12:28 +0200

    Render static catalog

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 
 const gameService = require('../services/gameService');
 
-
+router.get('/catalog', async (req, res) => {
+    res.render('games/catalog')
+})
 module.exports = router;
