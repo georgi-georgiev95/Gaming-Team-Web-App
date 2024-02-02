84d4244f8cbad76e040f30d5c71a070b349898de
Georgi957 <gesh007@abv.bg>
2024-02-02 10:08:37 +0200

    Add game controller to router

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const authController = require('./controllers/authController');
+const gameController = require('./controllers/gameController');
 
 router.use(homeController);
 router.use('/auth', authController);
+router.use('/games', gameController);
 router.get('*', (req, res) => {
     res.redirect('/404');
 })
