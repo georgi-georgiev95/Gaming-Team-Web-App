85689595a70de020f78f9112b5e9d3e4c949e0fd
Georgi957 <gesh007@abv.bg>
2024-02-02 11:27:29 +0200

    Buy game and show proper info

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -5,3 +5,17 @@ exports.create = (gameData) => Game.create(gameData);
 exports.getAll = () => Game.find();
 
 exports.getOne = (id) => Game.findById(id).populate('owner').populate('boughtBy');
+
+exports.edit = async (id, gameData) => {
+    const { name, image, price, description, genre, platform } = gameData;
+
+    if (!name || !image || !price || !description || !genre || !platform) {
+        throw new Error('All fields are required!');
+    }
+    await Game.findByIdAndUpdate(id, gameData);
+};
+
+exports.delete = (id) => Game.findByIdAndDelete(id);
+
+exports.buy = (id, userId) => Game.findByIdAndUpdate(id, {$push : {boughtBy: userId}});
+  
