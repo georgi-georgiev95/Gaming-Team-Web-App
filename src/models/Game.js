4ad03c01dcf7fd9c3d865f64c8840d93e09217f4
Georgi957 <gesh007@abv.bg>
2024-02-02 12:15:26 +0200

    Add validations to Game model

--- src/models/Game.js
+++ src/models/Game.js
@@ -5,22 +5,37 @@ const gameSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        minLength: [4, 'Name should be at least 4 characters long'],
     },
     image: {
         type: String,
         required: true,
+        validate: {
+            validator: function (value) {
+                return /^(http|https):\/\//.test(value);
+            },
+            message: 'Photo URL must start with http:// or https://'
+        }
     },
     price: {
         type: Number,
         required: true,
+        validate: {
+            validator: function (value) {
+                return value > 0;
+            },
+            message: `Price must be a positive number!`
+        }
     },
     description: {
         type: String,
         required: true,
+        minLength: [10, 'Description should be at least 10 characters long'],
     },
     genre: {
         type: String,
         required: true,
+        minLength: [2, 'Genre should be at least 2 characters long'],
     },
     platform: {
         type: String,
